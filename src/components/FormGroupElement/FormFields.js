1c18a573be41cfb7252e9cc79276f0f439fcabbd
Lauri Matti Saarni <lauri@web-essentials.asia>
2018-05-19 17:05:09 +0700

    [RFT] Remove duplicate code from FormElements
    Clean up some dirt.

--- src/components/FormGroupElement/FormFields.js
+++ src/components/FormGroupElement/FormFields.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import _ from 'lodash'
-import FormElement from './index'
+import { Field } from '../FormElement/FormFields'
 
 const propTypes = {
   value: PropTypes.string,
@@ -15,14 +14,6 @@ export const updateByPropertyName = (propertyName, value) => () => ({
 export const setStateValue = (propertyName, componentThis) => {
   return event => componentThis.setState(updateByPropertyName(propertyName, event.target.value))
 }
-const Field = ({ id, ...props }) => (
-  <FormElement {...props} name={id} id={id} label={props.label ? props.label : _.capitalize(id)} />
-)
-
-Field.propTypes = {
-  id: PropTypes.string,
-  label: PropTypes.string,
-}
 
 export const UsernameField = ({ onChange, value }) => (
   <Field className="" id="username" value={value} onChange={onChange} type="text" icon="user" />
@@ -31,26 +22,42 @@ export const UsernameField = ({ onChange, value }) => (
 UsernameField.propTypes = propTypes
 
 export const EmailField = ({ onChange, value }) => (
-  <Field className="py-2" value={value} id="email" onChange={onChange} type="text" placeholder="Email Address" icon="envelope" />
+  <Field
+    className="py-2"
+    value={value}
+    id="email"
+    onChange={onChange}
+    type="text"
+    placeholder="Email Address"
+    icon="envelope"
+  />
 )
 
 EmailField.propTypes = propTypes
 
 export const PasswordField = ({ onChange, value }) => (
-  <Field className="py-2" id="password" value={value} onChange={onChange} type="password" placeholder="Password" icon="lock" />
+  <Field
+    className="py-2"
+    id="password"
+    value={value}
+    onChange={onChange}
+    type="password"
+    placeholder="Password"
+    icon="lock"
+  />
 )
 
 PasswordField.propTypes = propTypes
 
 export const PasswordConfirmField = ({ onChangeOne, passwordOne, onChangeTwo, passwordTwo }) => (
   <div>
-    <Field 
-      className="" 
-      id="passwordOne" 
-      value={passwordOne} 
-      onChange={onChangeOne} 
-      type="password" 
-      placeholder="" 
+    <Field
+      className=""
+      id="passwordOne"
+      value={passwordOne}
+      onChange={onChangeOne}
+      type="password"
+      placeholder=""
       icon="lock"
     />
     <Field
