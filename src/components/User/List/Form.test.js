1dcd4fdf1f028545ab0a759f93d1d78d476604fb
Lauri Saarni <lauri.saarni@gmail.com>
2018-05-28 01:19:16 +0700

    Temporarily remove user list jest test

--- src/components/User/List/Form.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { UserList } from '../List'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<UserList loadUsers={() => {}} />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
